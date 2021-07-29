ea1be4df0ba5670f3f8e6b24afbc06fb5c059c64
kying-star <3077006505@qq.com>
2021-07-29 21:24:46 +0800

    fix: 修改router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import router from "./router/route"
-import Home from "./view/Home"
-import About from "./view/About";
-import Users from "./view/Users";
 
 function App() {
   console.log(router)
@@ -11,12 +8,14 @@ function App() {
     <Router className="App">
       <div className="App-header">
         <Switch>
-          <Route path="/about" component={About}>
-          </Route>
-          <Route path="/users" component={Users}>
-          </Route>
-          <Route path="/" component={Home}>
-          </Route>
+          {
+            router.map(e=>{
+              return (
+                  <Route exact path={e.path} component={e.component}>
+                  </Route>
+              )
+            })
+          }
         </Switch>
       </div>
     </Router>
